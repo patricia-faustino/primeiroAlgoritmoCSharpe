39637052d5487429c6401de1b33182251a9f3eb9
Patricia <patriciafaustinobr@gmail.com>
2020-09-26 21:26:00 -0300

    Adicionado a mensagem Olá Mundo

---
+++ PRIMEIRO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLÁ MUNDO!");
+        }
+    }
+}
